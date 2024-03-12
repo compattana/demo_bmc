a342b3c867e1570bff8e718c4334bc0ef0b96882
w3-devmaster <apichat_pep@hotmail.com>
2024-03-12 10:20:43 +0700

    update

--- config/loginForm.php
+++ config/loginForm.php
@@ -2,7 +2,7 @@
 
 return [
     'cover_image' => 'images/img.png',
-    'logo' => 'logo/BMC-logo.png',
+    'logo' => 'logo/300.png',
     'powerby' => '<a class="text-muted" target="_blank" href="https://compattana.com">Compattana Co.,Ltd.</a>',
     'copyright' => 'Copyright © 2022. All rights reserved.',
 ];
